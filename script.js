87d2d1ff692abab6db12a109f85d028533a0fd0d
Andrés <ajsalazar@uc.cl>
2023-06-13 18:56:12 -0400

    agregué el menu desplegable interactivo

--- script.js
+++ script.js
@@ -34,3 +34,15 @@ document.getElementById("desplegarMenu3").addEventListener("click", function (ev
 document.body.addEventListener("click", function () {
     ocultarTodosLosMenus();
 });
+
+
+
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    var menu = document.querySelector('.navegacion-principal ul');
+    if (menu.style.display === 'block') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'block';
+    }
+  });
+  
